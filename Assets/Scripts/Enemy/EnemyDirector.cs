a77e73605d8de9cf1bd37d68f4333f8eca9d34f2
TheTriggert <triggert@mail.ru>
2021-11-05 18:21:02 +0700

    Враги ходят (базово)
    Базовое движение врагов (пока ходят как по рельсам)

---
+++ Assets/Scripts/Enemy/EnemyDirector.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDirector : MonoBehaviour
+{
+    [SerializeField] List<NpcAndRoute> control = new List<NpcAndRoute>();
+    private void Start()
+    {
+        for (int i = 0; i < control.Count; i++) SetupNpcOnRoute(control[i].npc, control[i].route);
+    }
+    public void MoveNPCByRoute(EnemyBase npc, NPCRoute route) {
+        if (npc == null) return;
+        var controlIndex = control.FindIndex((con) => { return con.npc == npc; });
+        if (controlIndex >= 0) control.RemoveAt(controlIndex);
+        SetupNpcOnRoute(npc, route);
+        if (route != null) control.Add(new NpcAndRoute(npc, route));
+    }
+    void SetupNpcOnRoute(EnemyBase npc, NPCRoute route) {
+        if (npc == null) return;
+        if (route == null) {
+            npc.SetTarget(null);
+            npc.OnTargetReached = default;
+            return;
+        }
+        npc.SetTarget(route.GetNextPoint(npc.Target));
+        npc.OnTargetReached = GiveNextTarget;
+    }
+    void GiveNextTarget(EnemyBase npc) {
+        //Debug.Log("trying to give new point...");
+        int npcIndex = 0;
+        for (npcIndex = 0; npcIndex < control.Count; npcIndex++) if (control[npcIndex].npc == npc) break;
+        if (npcIndex < control.Count)
+        {
+            npc.SetTarget(control[npcIndex].route.GetNextPoint(npc.Target));
+        }
+        else {
+            Debug.Log("No next point for " + npc.gameObject.name);
+        }
+    }
+}
+[System.Serializable]
+public class NpcAndRoute {
+    public EnemyBase npc;
+    public NPCRoute route;
+    public NpcAndRoute() { }
+    public NpcAndRoute(EnemyBase newNpc, NPCRoute newRoute) { npc = newNpc; route = newRoute; }
+} 
