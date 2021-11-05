070d54e84f808b0816b5b9e38da2ea09d0674535
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2021-11-05 17:50:32 +0700

    Rooms determine by coordinates

--- Assets/Scripts/Rooms/Rooms.cs
+++ Assets/Scripts/Rooms/Rooms.cs
@@ -1,49 +1,46 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-
-namespace Rooms
+public class Rooms : MonoBehaviour
 {
-    public class Rooms : MonoBehaviour
+    private List<Tilemap> roomsTilemaps;
+
+    private void Awake()
     {
-        private static List<Tilemap> roomsTilemaps;
-
-        private void Awake()
+        for (var i = 0; i < transform.childCount; i++)
         {
-            roomsTilemaps = new List<Tilemap>();
-            for (var i = 0; i < transform.childCount; i++)
+            if (transform.GetChild(i).TryGetComponent(out Tilemap tm))
             {
-                if (transform.GetChild(i).TryGetComponent(out Tilemap tm))
-                {
-                    roomsTilemaps.Add(tm);
-                }
+                roomsTilemaps.Add(tm);
             }
         }
+    }
 
-        public static string GetRoomName(Vector3 coords)
+    public string GetRoomName(Vector3 coords)
+    {
+        string ans = null;
+        foreach (Tilemap tm in roomsTilemaps)
         {
-            string ans = null;
-            foreach (Tilemap tm in roomsTilemaps)
+            if (tm.GetTile(tm.WorldToCell(coords)) != null)
             {
-                if (tm.GetTile(tm.WorldToCell(coords)) != null)
-                {
-                    ans = tm.gameObject.name;
-                    break;
-                }
-
+                ans = tm.gameObject.name;
+                break;
             }
 
-            return ans;
         }
+
+        return ans;
+    }
     
-        public static string GetRoomName(GameObject obj)
-        {
-            return GetRoomName(obj.transform);
-        }
+    public string GetRoomName(GameObject obj)
+    {
+        return GetRoomName(obj.transform);
+    }
     
-        public static string GetRoomName(Transform tr)
-        {
-            return GetRoomName(tr.position);
-        }
+    public string GetRoomName(Transform tr)
+    {
+        return GetRoomName(tr.position);
     }
 }
