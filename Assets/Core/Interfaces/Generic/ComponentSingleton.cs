df8e2bda945a1c5534e50d607eb758a6106550a5
Vladimir Savkin <v.savkin@trueengineering.ru>
2021-11-05 18:16:59 +0700

    feat: Add Core folder with base singeltons class

---
+++ Assets/Core/Interfaces/Generic/ComponentSingleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace MurphyInc.Core.Interfaces.Generic
+{
+    public class ComponentSingleton<T> 
+        : Component where T : Component
+    {
+        private static readonly object _lock = new();
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    instance ??= FindObjectOfType<T>();
+
+                    if (instance == null)
+                    {
+                        var obj = new GameObject("SingletonOf" + typeof(T).Name);
+                        instance = obj.AddComponent<T>();
+                        DontDestroyOnLoad(obj);
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
