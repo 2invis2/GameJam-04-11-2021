d90065b142db165510e764640107f786bb12daaf
Vladimir Savkin <v.savkin@trueengineering.ru>
2021-11-06 23:40:05 +0700

    feat: Add ActionFeatures in storages:
    Тебя не замечают пока стоишь
    Отскакивающие снаряды
    Двери открываются быстрее
    Все двери открыты
    Босс выходит из комнаты
    Дырявый пол
    Противников можно толкать

---
+++ Assets/FeatureStorages/FeatureStorageBoss.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using MurphyInc.Core.Model;
+
+namespace Assets.Scripts.FeatureStorages
+{
+
+    public sealed class FeatureStorageBoss
+    {
+        private static readonly FeatureStorage instance;
+
+        static FeatureStorageBoss()
+        {
+            var features = new[]
+            {
+                 BossIsWalking
+            };
+
+            instance = new FeatureStorage(features);
+        }
+
+        private FeatureStorageBoss() { }
+
+        public static FeatureStorage Instance => instance;
+
+        public static IEnumerable<ActionFeature> Actions => instance.Features;
+
+        public static readonly ActionFeature BossIsWalking = new ActionFeature(name: nameof(BossIsWalking), description: "Босс выходит из комнаты", isEnable: false);       
+    }
+}
--- Assets/FeatureStorages/FeatureStorageEnemy.cs
+++ Assets/FeatureStorages/FeatureStorageEnemy.cs
@@ -24,7 +24,8 @@ namespace Assets.Scripts.FeatureStorages
 
         public static IEnumerable<ActionFeature> Actions => instance.Features;
 
-        public static readonly ActionFeature RandVelocity = new ActionFeature(name: nameof(RandVelocity), description: "Никто не контролируют свою скорость", isEnable: true);
-        public static readonly ActionFeature ReactToMovement = new ActionFeature(name: nameof(ReactToMovement), description: "Тебя не замечают пока стоишь", isEnable: true);
+        public static readonly ActionFeature RandVelocity = new ActionFeature(name: nameof(RandVelocity), description: "Никто не контролируют свою скорость", isEnable: false);
+        public static readonly ActionFeature ReactToMovement = new ActionFeature(name: nameof(ReactToMovement), description: "Тебя не замечают пока стоишь", isEnable: false);
+        public static readonly ActionFeature CanBePushed = new ActionFeature(name: nameof(CanBePushed), description: "Противников можно толкать", isEnable: false);        
     }
 }
