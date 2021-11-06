a025de687ca8c728dc8875548519b58a0a881d1a
TheTriggert <triggert@mail.ru>
2021-11-06 20:19:40 +0700

    Навмеш для врагов
    MovementComponent переписан для навмеша, старый - теперь old

--- Assets/Scripts/Enemy/EnemyChaser.cs
+++ Assets/Scripts/Enemy/EnemyChaser.cs
@@ -6,20 +6,23 @@ public class EnemyChaser : EnemyBase
 {
     bool isChasing = false;
     [SerializeField] float chaseSpeed;
+    Transform chaseTarget;
     protected override void DetectionResult(List<GameObject> foundObjects)
     {
         base.DetectionResult(foundObjects);
-        if (isChasing) { 
-
+        if (isChasing) {
+            movement.SetTarget(chaseTarget);
         }
         else
         {
             if (foundObjects.Count > 0) {
                 isChasing = true;
-                movement.SetBasicMovementType(BasicMovementType.TurnAndMove);
+                //movement.SetBasicMovementType(BasicMovementType.TurnAndMove);
                 if (chaseSpeed != default) movement.MoveSpeed = chaseSpeed;
+                movement.Agent.acceleration = chaseSpeed * 3f;
                 EnemyDirector.Instance.UnregisterEnemy(this);
-                movement.SetTarget(foundObjects[0].transform);
+                //movement.SetTarget(foundObjects[0].transform);
+                chaseTarget = foundObjects[0].transform;
             }
         }
     }
