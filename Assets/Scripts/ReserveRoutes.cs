26bcdef721578e1a3a51588522e7e28d8d097e2d
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2021-11-07 17:47:21 +0700

    fix

--- Assets/Scripts/ReserveRoutes.cs
+++ Assets/Scripts/ReserveRoutes.cs
@@ -9,17 +9,7 @@ public class ReserveRoutes : PseudoSingletonMonoBehaviour<ReserveRoutes>
 {
     [SerializeField] private List<NPCRoute> reservsInspector;
     private static List<NPCRoute> reservs;
-
-    void Awake()
-    {
-        Invoke(nameof(Test), 2f);
-        reservs = reservsInspector;
-    }
-
-    void Test()
-    {
-        FeatureStorageRooms.Instance.GetByName("RestrictedAccessServerRoom").IsEnable = true;
-    }
+    
     public static void SetReserveRoute(EnemyBase enemyBase)
     {
         var randomIndex = Random.Range(0, reservs.Count-1);
