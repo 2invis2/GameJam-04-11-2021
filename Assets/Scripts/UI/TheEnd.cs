a73a4ec2be27bb8feebb6d2a5fa9d71255d03df3
Oleg Bavykin <40769313+2invis2@users.noreply.github.com>
2021-11-07 23:58:37 +0700

    add Final scene

---
+++ Assets/Scripts/UI/TheEnd.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheEnd : MonoBehaviour
+{
+    public void TheEndClick()
+    {
+        Application.Quit();
+    }
+}
