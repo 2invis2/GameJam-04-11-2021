2b0d175a8268643748d53311e841f1746a3396cb
Vladimir Savkin <v.savkin@trueengineering.ru>
2021-11-07 19:56:06 +0700

    feat: KnownActionFeatures in FeatureStorageMain

--- Assets/FeatureStorages/FeatureStorageMain.cs
+++ Assets/FeatureStorages/FeatureStorageMain.cs
@@ -10,25 +10,29 @@ namespace Assets.Scripts.FeatureStorages
     {
         private static readonly FeatureStorage instance;
 
-        static FeatureStorageMain()
+        static FeatureStorageMain()   
         {
-            var features = new ActionFeature[]
-            {
-                 
-            };
 
-            instance = new FeatureStorage(features);
         }
 
         private FeatureStorageMain() { }
 
-        public static FeatureStorage Instance => instance;
+        public static FeatureStorage Instance => new FeatureStorage(new ActionFeature[] {});
 
         public static IEnumerable<ActionFeature> Features => instance.Features;
 
         public static IEnumerable<IFeature> GetFeatures(bool isEnable, bool isAvailable)
         {
-           return instance.Features.Where(x => x.IsEnable == isEnable && x.IsAvailable == isAvailable);
+            return instance.Features.Where(x => x.IsEnable == isEnable && x.IsAvailable == isAvailable);
         }
+
+        public static  readonly Dictionary<string, IEnumerable<ActionFeature>> KnownActionFeatures = new Dictionary<string, IEnumerable<ActionFeature>>
+            {
+                { nameof(FeatureStorageEnemy), FeatureStorageEnemy.Features },
+                { nameof(FeatureStorageEnv), FeatureStorageEnv.Features },
+                { nameof(FeatureStorageRooms), FeatureStorageRooms.Features },
+                { nameof(FeatureStorageProjectile), FeatureStorageEnemy.Features },
+                { nameof(FeatureStorageEnemy), FeatureStorageEnemy.Features },
+            };
     }
 }
