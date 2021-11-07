66658f291c0b2d3e6a73861aa51891735e037514
Oleg Bavykin <40769313+2invis2@users.noreply.github.com>
2021-11-07 23:09:35 +0700

    fix Intro

---
+++ Assets/Scripts/UI/FinalIntro.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalIntro : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource intro;
+
+    void Update()
+    {
+        Debug.Log(intro.time);
+        if (intro.time >= 126.0f)
+        {
+            OpenNextScene();
+        }
+    }
+
+    public void OpenNextScene()
+    {
+        Application.LoadLevel(1);
+    }
+}
